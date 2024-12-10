bccafd8945bb67b6db8228f082b903507422d36a
Andrea Freddi <56250515+AndreaFreddiGo@users.noreply.github.com>
2024-12-10 16:44:00 +0100

    Adds new corrections

--- src/redux/actions/index.js
+++ src/redux/actions/index.js
@@ -1,5 +1,10 @@
 export const ADD_TO_FAVS = 'ADD_TO_FAVS'
 export const REMOVE_FROM_FAVS = 'REMOVE_FROM_FAVS'
+export const GET_JOBS = 'GET_JOBS'
+export const GET_JOBS_ERROR = 'GET_JOBS_ERROR'
+
+const baseEndpoint = 'https://strive-benchmark.herokuapp.com/api/jobs?search='
+
 
 export const addToFavsAction = (data) => {
   return {
@@ -14,3 +19,30 @@ export const removeFromFavsAction = (i) => {
     payload: i,
   }
 }
+
+export const getJobs = () => {
+  return async (e, dispatch, getState) => {
+    e.preventDefault()
+    // qui dentro potete fare anche operazioni asincrone (Promise)
+  }
+
+    try {
+      const response = await fetch(baseEndpoint + query + '&limit=20')
+      if (response.ok) {
+        const fetchedJobs = await response.json()
+        dispatch({
+          type: 'GET_JOBS',
+          payload: fetchedJobs,
+        })
+      } else {
+        throw new Error('Error fetching results')
+      }
+    } catch (error) {
+      console.log(error)
+      dispatch({
+        type: 'GET_JOBS_ERROR',
+        payload: error,
+      })
+    }
+  }
+
